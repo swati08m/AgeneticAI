9e58237b405d0d396da60ceea7936a36a401fc7b
Swati <swatijadhav0000@gmail.com>
2026-06-18 19:58:04 +0530

    chatbot practice

--- Chatbot/Basic_chatbot/basic_chatbot_frontend.py
+++ Chatbot/Basic_chatbot/basic_chatbot_frontend.py
@@ -1,4 +1,7 @@
+# streamlit run basic_chatbot_frontend.py --server.fileWatcherType none
+
 import streamlit as st
+from basic_chatbot_backend import workflow
 
 with st.sidebar:
     if st.button("Clear Chat"):
@@ -11,7 +14,7 @@ assistant_role = "assistant"
 
 st.title("Basic Chatbot")
 
-# Store message history in session state
+
 # Check if 'message_history' key exists in session state, if not initialize it as an empty list
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
@@ -29,8 +32,36 @@ else:
 # input box for user to put query
 user_input = st.chat_input('user')
 if user_input:
-    st.session_state['message_history'].append({'role': user_role, 'content': user_input})
     # box to show message
     with st.chat_message(user_role):
         # put user input in the box
         st.text(user_input)
+    initial_state = {'user_query': user_input}
+    '''
+    # without streaming
+    
+    response = workflow.invoke(initial_state)
+    ai_reply = response['llm_response']
+    print('ai_reply', ai_reply)
+
+    st.session_state['message_history'].append({'role': assistant_role, 'content': ai_reply})
+
+    with st.chat_message(assistant_role):
+        # put user input in the box
+        st.text(ai_reply)
+    
+    '''
+
+    #  with streaming
+    with st.chat_message(assistant_role):
+        response_placeholder = st.empty()
+        full_response = ""
+
+        for chunk, metadata in workflow.stream(initial_state, stream_mode = "messages"):
+            if hasattr(chunk, 'content'):
+                full_response+=chunk.content
+                # response_placeholder.text(full_response)
+
+    # Store message history in session state
+    st.session_state['message_history'].append({'role': user_role, 'content': user_input})
+    st.session_state['message_history'].append({'role': assistant_role, 'content': full_response})
